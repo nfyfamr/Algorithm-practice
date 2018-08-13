e493275857f6734333c4114f2a69085585b8e6a3
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 03:14:13 +0900

    5339 - trial 1

---
+++ www.acmicpc.net/5339/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main (int argc, char** argv)
+{
+	std::cout << "     /~\\\n";
+	std::cout << "    ( oo|\n";
+	std::cout << "    _\\=/_\n";
+	std::cout << "   /  _  \\\n";
+	std::cout << "  //|/.\\|\\\\\n";
+	std::cout << " ||  \\ /  ||\n";
+	std::cout << "============\n";
+	std::cout << "|          |\n";
+	std::cout << "|          |\n";
+	std::cout << "|          |\n";
+
+	return 0;
+}
