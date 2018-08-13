63192d8517c21d6a88f4131eeeec49ae31222349
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 03:05:47 +0900

    5338 - trial 1

---
+++ www.acmicpc.net/5338/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main (int argc, char** argv)
+{
+    std::cout << "       _.-;;-._\n";
+    std::cout << ".-..-'|   ||   |\n";
+    std::cout << ".-..-'|_.-;;-._|\n";
+    std::cout << ".-..-'|   ||   |\n";
+    std::cout << ".-..-'|_.-''-._|\n";
+
+    return 0;
+}
