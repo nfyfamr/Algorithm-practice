a46f165d39cf56d752b0ed102219738dd268264d
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 02:40:23 +0900

    5337 - trial 1

---
+++ www.acmicpc.net/5337/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main (int argc, char** argv)
+{
+	std::cout << ".  .   .\n";
+	std::cout << "|  | _ | _. _ ._ _  _\n";
+	std::cout << "|/\\|(/.|(_.(_)[ | )(/.\n";
+
+	return 0;
+}
