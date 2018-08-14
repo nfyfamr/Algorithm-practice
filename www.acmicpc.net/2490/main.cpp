0a53d4f067e277393b924fda1594d9c4a0d58bd2
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 17:51:04 +0900

    2490 - trial 1

---
+++ www.acmicpc.net/2490/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main(int argc, char** argv)
+{
+    for (int i = 0; i < 3; ++i)
+    {
+        int a, b, c, d;
+        int sum;
+        
+        scanf("%d %d %d %d", &a, &b, &c, &d);
+        sum = a + b + c + d;
+        printf("%c", (sum == 0) ? 'E' : 'D' - sum);
+    }
+    
+    return 0;
+}
