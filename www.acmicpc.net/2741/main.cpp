9af306f03730d73b8f0cde3c3962dfd0b3e1cf6c
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 23:18:34 +0900

    2741 - trial 1

---
+++ www.acmicpc.net/2741/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    int a;
+    cin >> a;
+    
+    for (int i = 1; i < a+1; ++i) {
+        cout << i << endl;
+    }
+    
+    return 0;
+}
