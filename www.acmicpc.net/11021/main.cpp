795f749dffe53df9b2fb4cb228429c094f54ed29
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-15 00:52:09 +0900

    11021 - trial 1

---
+++ www.acmicpc.net/11021/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int T;
+    
+    cin >> T;
+    for (int i = 0; i < T; ++i)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case #" << i+1 << " : " << a + b << endl;
+    }
+    
+    return 0;
+}
