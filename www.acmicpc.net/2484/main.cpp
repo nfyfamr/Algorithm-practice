6524bfa3d811767aed24e9ce7501edb27d383cc2
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 17:58:45 +0900

    2484 - trial 3

--- www.acmicpc.net/2484/main.cpp
+++ www.acmicpc.net/2484/main.cpp
@@ -30,6 +30,7 @@ int GetReward (int *t)
 {
     int dice[7] = {0, };
     int reward = 0;
+    bool e = false;
     
     for (int i=0; i<4; ++i)
     {
@@ -40,10 +41,10 @@ int GetReward (int *t)
     {
         switch (dice[i])
         {
-            case 4: reward += 50000 + i*5000; break;
-            case 3: reward += 10000 + i*1000; break;
-            case 2: reward += 2000  + i*500; break;
-            case 1: reward  =         i*600; break;
+            case 4: e = true; reward += 50000 + i*5000; break;
+            case 3: e = true; reward += 10000 + i*1000; break;
+            case 2: e = true; reward += 2000  + i*500; break;
+            case 1: if (!e)   reward  =         i*600; break;
         }
     }
     
