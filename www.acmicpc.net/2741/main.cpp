ebf813e1f266e3e5dd5c8465087ebc6b32e3ddb3
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 23:18:51 +0900

    2741 - trial 2

--- www.acmicpc.net/2741/main.cpp
+++ www.acmicpc.net/2741/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <sstream>
+
 using namespace std;
 
 int main(int argc, char** argv) {
     int a;
+    ostringstream os;
+    
     cin >> a;
     
     for (int i = 1; i < a+1; ++i) {
-        cout << i << endl;
+        os << i << '\n';
     }
+    cout << os.str();
     
     return 0;
 }
