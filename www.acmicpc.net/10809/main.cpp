61de21494c9770f2415d119e21897f0a44906cd2
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-15 00:36:31 +0900

    10809 - trial 1

---
+++ www.acmicpc.net/10809/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    array<int, 26> alpha;
+    string str;
+    ostream_iterator<int, char> out_iter(cout, " ");
+    
+    alpha.fill(-1);
+    cin >> str;
+    for (auto it = str.begin(); it != str.end(); ++it)
+    {
+        if (alpha[*it - 'a'] == -1)
+        {
+            alpha[*it - 'a'] = distance(str.begin(), it);
+        }
+    }
+    copy(alpha.begin(), alpha.end(), out_iter);
+    cout << endl;
+    
+    return 0;
+}
