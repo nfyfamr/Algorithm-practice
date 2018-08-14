1975c4f10524e07a1e3d32c74d9a7165e223e267
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 11:34:40 +0900

    1149 - trial 1

---
+++ www.acmicpc.net/1149/main.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int N;
+    int RGB[1000][3];
+    
+    scanf("%d", &N);
+    scanf("%d %d %d", &RGB[0][0], &RGB[0][1], &RGB[0][2]);
+    for (int i=1; i<N; ++i)
+    {
+        int r, g, b;
+        scanf("%d %d %d", &r, &g, &b);
+        
+        RGB[i][0] = r + (RGB[i - 1][1] < RGB[i - 1][2] ? RGB[i - 1][1] : RGB[i - 1][2]);
+        RGB[i][1] = g + (RGB[i - 1][0] < RGB[i - 1][2] ? RGB[i - 1][0] : RGB[i - 1][2]);
+        RGB[i][2] = b + (RGB[i - 1][0] < RGB[i - 1][1] ? RGB[i - 1][0] : RGB[i - 1][1]);
+    }
+    printf("%d\n", *min_element(&RGB[N - 1][0], &RGB[N - 1][2]));
+    
+    return 0;
+}
