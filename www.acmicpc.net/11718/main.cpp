0f2650b36250bce49dd9769e12f168f111c19eaa
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-15 00:57:50 +0900

    11718 - trial 1

---
+++ www.acmicpc.net/11718/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string str;
+    
+    while (cin >> str)
+    {
+        cout << str << endl;
+    }
+    
+    return 0;
+}
