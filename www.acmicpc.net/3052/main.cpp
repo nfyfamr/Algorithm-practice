c72ebece6e7a2c511c166157fd9f1dc5e328ecf4
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 23:43:41 +0900

    3052 - trial 2

--- www.acmicpc.net/3052/main.cpp
+++ www.acmicpc.net/3052/main.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
 #include <bitset>
-#include <unistd.h>
-#include <sys/mman.h>
 
 using namespace std;
 
-bitset<42> bs;
-
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    char *buf = (char *)mmap(NULL, 10010, PROT_READ, MAP_PRIVATE, 0, 0);
+    bitset<42> bs;
+    int i, val;
     
-    int i, cnt = 0, num = 0, mod = 0;
-    for (i = 0; cnt < 10; ++i)
+    for (i = 0; i < 10; ++i)
     {
-        if (buf[i] > 40)
-            num = 10 * num + (buf[i] & 0x0f);
-        else
-        {
-            bs.set(num % 42);
-            ++cnt;
-        }
+        cin >> val;
+        bs.set(val % 42);
     }
     
     cout << bs.count() << endl;
