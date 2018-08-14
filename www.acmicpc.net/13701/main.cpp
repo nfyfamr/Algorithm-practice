af83ccef03383220b74a0977dd472733051c26ca
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-15 01:15:14 +0900

    13701 - trial 5

--- www.acmicpc.net/13701/main.cpp
+++ www.acmicpc.net/13701/main.cpp
@@ -6,9 +6,8 @@ int main(int argc, char** argv)
     std::bitset<33554432> bs;
     int i;
     
-    while (!std::cin.eof())
+    while (std::cin >> i && std::cin.eof())
     {
-        std::cin >> i;
         if (!bs.test(i))
         {
             std::cout << i << " ";
