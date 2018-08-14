88933910e9cafda667382317637c613b49e64c47
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-15 01:17:10 +0900

    14581 - trial 1

---
+++ www.acmicpc.net/14581/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main (int argc, char** argv)
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    
+    char id[21];
+    
+    std::cin >> id;
+    std::cout << ":fan::fan::fan:\n:fan::" << id << "::fan:\n:fan::fan::fan:\n";
+    
+    return 0;
+}
