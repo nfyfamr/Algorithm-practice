ef4994a9e17b82aae4ed563bfd49b55a1813bd7f
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 17:49:52 +0900

    2455 - trial 2

---
+++ www.acmicpc.net/2455/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+
+int main (int argc, char** argv)
+{
+    int arr[8];
+    
+    std::cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4] >> arr[5] >> arr[6] >> arr[7];
+    std::cout << std::max(std::max(arr[1], arr[1]-arr[2]+arr[3]), arr[6]) << std::endl;
+    
+    return 0;
+}
