facc11fa3111fa95d62732695d9774b24b795dd8
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-15 01:14:07 +0900

    13701 - trial 1

---
+++ www.acmicpc.net/13701/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bitset>
+
+int main(int argc, char** argv)
+{
+    std::bitset<33554432> bs;
+    int i;
+    
+    while (std::cin >> i)
+    {
+        if (!bs.test(i))
+        {
+            std::cout << i << " ";
+            bs.set(i);
+        }
+    }
+    
+    return 0;
+}
