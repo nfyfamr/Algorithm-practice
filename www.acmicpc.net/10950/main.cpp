193538e6ec31362d132f01dad842e7f3df5aa3e9
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-15 00:48:36 +0900

    10950 - trial 1

---
+++ www.acmicpc.net/10950/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int T;
+    
+    cin >> T;
+    while( T-- >0 )
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    
+    return 0;
+}
