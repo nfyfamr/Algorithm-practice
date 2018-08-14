84c3f8d01457279a7c67d02e883a0e3af028b974
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-15 00:25:44 +0900

    9012 - trial 3

---
+++ www.acmicpc.net/9012/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int T;
+    
+    cin >> T;
+    while (T-- > 0)
+    {
+        int t = 0;
+        string str;
+        
+        cin >> str;
+        for (auto it = str.begin(); (it != str.end()) && (t >= 0) ; ++it)
+        {
+            if (*it == '(')
+                ++t;
+            else
+                --t;
+        }
+        
+        cout << (t == 0 ? "YES" : "NO") << endl;
+    }
+    
+    return 0;
+}
