84025bd325d6c1ff4c5e3487f7af29190a5738a5
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 23:44:46 +0900

    4344 - trial 2

---
+++ www.acmicpc.net/4344/main.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int C;
+    
+    scanf("%d", &C);
+    while (C-- > 0)
+    {
+        int N;
+        int sum, avr;
+        int points[1000] = { 0 };
+        double numOfOverAverage;
+        
+        sum = 0;
+        scanf("%d", &N);
+        for (int i = 0; i < N; ++i)
+        {
+            scanf("%d", &points[i]);
+            sum += points[i];
+        }
+        
+        avr = sum / N;
+        numOfOverAverage = count_if(points, points + N, [&avr](int e) -> bool {
+            return e > avr;
+        });
+        
+        printf("%.3f%%\n", numOfOverAverage/N*100);
+    }
+    return 0;
+}
