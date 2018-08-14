3e78bc0d16544403d8596c6137ba668d6455c4e1
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 17:39:09 +0900

    2193 - trial 1

---
+++ www.acmicpc.net/2193/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int N;
+    int p[90][2];
+    
+    p[0][0] = 0;
+    p[0][1] = 1;
+    scanf("%d", &N);
+    for (int i = 1; i < N; ++i)
+    {
+        p[i][0] = p[i - 1][0] + p[i - 1][1];
+        p[i][1] = p[i - 1][0];
+    }
+    printf("%d\n", p[N - 1][0] + p[N - 1][1]);
+    
+    return 0;
+}
