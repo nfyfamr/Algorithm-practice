c3250facdc0de8e044bdc6aec71fcf7f4dad635c
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-15 01:16:14 +0900

    13701 - trial 9

--- www.acmicpc.net/13701/main.cpp
+++ www.acmicpc.net/13701/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <bitset>
-#include <ios>
 
 int main(int argc, char** argv)
 {
-    ios_base::sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
     
     std::bitset<33554432> bs;
