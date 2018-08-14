cb7f71473e5a9157451cf0af0522471487f31dde
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-14 11:36:56 +0900

    1152 - trial 2

--- www.acmicpc.net/1152/main.cpp
+++ www.acmicpc.net/1152/main.cpp
@@ -8,7 +8,14 @@ int main(int argc, char** argv)
     std::string str;
     
     std::getline(std::cin, str);
-    count = std::count(str.begin(), str.end(), ' ');
+    
+    std::string::iterator itBegin = str.begin();
+    std::string::iterator itEnd = str.end()-1;
+    
+    for (; *itBegin == ' '; ++itBegin);
+    for (; *itEnd == ' '; --itEnd);
+    
+    count = std::count(itBegin, itEnd, ' ');
     std::cout << count+1 << std::endl;
     
     return 0;
