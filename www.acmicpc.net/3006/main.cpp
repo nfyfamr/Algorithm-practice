602813d015d5b3cfee02e1fb51461b5e570348bb
Yongjae Lee <yongjae.lee@wrl.onl>
2018-08-16 00:12:26 +0900

    3006 - trial 4

--- www.acmicpc.net/3006/main.cpp
+++ www.acmicpc.net/3006/main.cpp
@@ -1,5 +1,5 @@
 #include <cstdio>
-#include <vector>
+#include <list>
 #include <iterator>
 
 int main (int argc, char** argv)
@@ -7,15 +7,13 @@ int main (int argc, char** argv)
 	int N;
 	int j;
 	int count;
-	std::vector<int> v;
-
-	v.reserve(100000);
+	std::list<int> list;
 
 	scanf("%d", &N);
 	for (int i=0; i<N; ++i) 
 	{
 		scanf("%d", &j);
-		v.push_back(j);
+		list.push_back(j);
 	}
 
 	for (int i=0; i<N; ++i)
@@ -23,23 +21,25 @@ int main (int argc, char** argv)
 		count = 0;
 		if (i%2 == 0)
 		{
-			auto it = v.begin();
+			auto it = list.begin();
 			while (*it != i/2+1)
 			{
 				++it;
 				++count;
 			}
-			v.erase(it);
+			list.erase(it);
 		}
 		else
 		{
-			auto it = v.rbegin();
+			auto it = list.rbegin();
 			while (*it != N-i/2)
 			{
 				++it;
 				++count;
 			}
-			v.erase(it.base()-1);
+			auto b = it.base();
+			--b;
+			list.erase(b);
 		}
 		printf("%d\n", count);
 	}
