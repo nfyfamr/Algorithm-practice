8b532692a654d8c60412c8513f9b5015a62bbc26
Yongjae Lee <nfyfamraa@gmail.com>
2023-12-08 12:10:22 -0500

    25083 - trial 1

---
+++ www.acmicpc.net/25083/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cout << "         ,r'\"7" << endl;
+    cout << "r`-_   ,'  ,/" << endl;
+    cout << " \\. \". L_r'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+}
