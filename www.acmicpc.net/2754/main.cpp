7438ff5d459a4e6e98e114298f78fcf8654e72ec
Yongjae Lee <nfyfamraa@gmail.com>
2023-12-10 06:47:14 -0500

    2754 - trial 1

---
+++ www.acmicpc.net/2754/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    char g;
+    float s;
+    cin >> g;
+    s = 4.0 - (g - 'A');
+    cin >> g;
+    s += g == '+' ? 0.3 : (g == '-' ? -0.3 : 0);
+
+    cout << fixed;
+    cout << setprecision(1);
+    cout << s;
+}
