fa3ca90be0c6ffe69cc877c70090be6c38bd1164
Yongjae Lee <nfyfamraa@gmail.com>
2023-12-10 17:26:04 -0500

    2231 - trial 1

---
+++ www.acmicpc.net/2231/main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int num_gen(int x)
+{
+    int rst = x;
+    while (x / 10 > 0)
+    {
+        rst += x % 10;
+        x = x / 10;
+    }
+    return rst + x;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, power = 1;
+    cin >> n;
+    int t = n;
+    while (t / 10 > 0){
+        ++power;
+        t = t % 10;
+    }
+
+    for (int i = (n > 19 ? n - 10*power : 1); i < n; ++i)
+    {
+        if (num_gen(i) == n)
+        {
+            cout << i;
+            break;
+        }
+    }
+}
