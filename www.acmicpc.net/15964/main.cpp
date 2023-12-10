7b60f5d960d3efa124a0dbf2d4876244ef1d93b1
Yongjae Lee <nfyfamraa@gmail.com>
2023-12-10 07:07:19 -0500

    15964 - trial 1, implementation

---
+++ www.acmicpc.net/15964/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long a, b;
+    cin >> a >> b;
+    cout << (a + b) * (a - b);
+}
