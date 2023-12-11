0d229ef52e9e3887a4db53e032dd8dfc187a3232
Yongjae Lee <nfyfamraa@gmail.com>
2023-12-10 23:48:33 -0500

    1676 - trial 2, implementation

--- www.acmicpc.net/1676/main.cpp
+++ www.acmicpc.net/1676/main.cpp
@@ -6,18 +6,8 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
+    int n, cnt;
     cin >> n;
-    int cnt = 0, _f = 1;
-    for (int i = 1; i < n + 1; ++i)
-    {
-        _f *= i;
-        while (_f % 10 == 0)
-        {
-            _f = _f / 10;
-            ++cnt;
-        }
-        _f = _f % 10;
-    }
+    cnt = (n / 5) + (n / 25) + (n / 125);
     cout << cnt;
 }
