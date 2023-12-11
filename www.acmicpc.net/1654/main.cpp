b3ef81f2e8919285070ac0a75cc67c9c1fb1dce9
Yongjae Lee <nfyfamraa@gmail.com>
2023-12-10 23:13:03 -0500

    1654 - trial 2, parametric search

--- www.acmicpc.net/1654/main.cpp
+++ www.acmicpc.net/1654/main.cpp
@@ -20,13 +20,14 @@ int main()
     vector<int> cables(n);
     for (int i = 0; i < k; ++i) cin >> cables[i];
 
-    int low = 1, high = *max_element(cables.begin(), cables.end());
-    do
+    int low = 1, mid, high = *max_element(cables.begin(), cables.end());
+    int cutted;
+    while (low <= high)
     {
-        int mid = (low + high) / 2;
-        int cutted = cutting(mid, cables);
-        if (cutted >= n) low = mid;
-        else if (cutted < n) high = mid;
-    } while (low < high);
+        mid = (low + high) / 2;
+        cutted = cutting(mid, cables);
+        if (cutted >= n) low = mid + 1;
+        else high = mid - 1;
+    }
     cout << high;
 }
