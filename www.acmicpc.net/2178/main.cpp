6c94ad960f43964671f852cfbaa22051166fd17c
Yongjae Lee <nfyfamraa@gmail.com>
2023-12-12 17:30:43 -0500

    2178 - trial 2, dp, bfs

--- www.acmicpc.net/2178/main.cpp
+++ www.acmicpc.net/2178/main.cpp
@@ -30,32 +30,36 @@ int main()
 
     vector<vector<int>> dp(n, vector<int>(m, 10000));
     dp[0][0] = 1;
+    labyrinth[0][0] = 0;
     while (!stack.empty())
     {
         while(!stack.empty())
         {
             auto p = stack.back();
             stack.pop_back();
-            labyrinth[p.first][p.second] = 0;
             if (p.first > 0 && labyrinth[p.first - 1][p.second] == 1)
             {
                 next_stack.push_back({p.first - 1, p.second});
                 dp[p.first - 1][p.second] = min(dp[p.first - 1][p.second], dp[p.first][p.second] + 1);
+                labyrinth[p.first - 1][p.second] = 0;
             }
             if (p.second > 0 && labyrinth[p.first][p.second - 1] == 1)
             {
                 next_stack.push_back({p.first, p.second - 1});
                 dp[p.first][p.second - 1] = min(dp[p.first][p.second - 1], dp[p.first][p.second] + 1);
+                labyrinth[p.first][p.second - 1] = 0;
             }
             if (p.first < n - 1 && labyrinth[p.first + 1][p.second] == 1)
             {
                 next_stack.push_back({p.first + 1, p.second});
                 dp[p.first + 1][p.second] = min(dp[p.first + 1][p.second], dp[p.first][p.second] + 1);
+                labyrinth[p.first + 1][p.second] = 0;
             }
             if (p.second < m - 1 && labyrinth[p.first][p.second + 1] == 1)
             {
                 next_stack.push_back({p.first, p.second + 1});
                 dp[p.first][p.second + 1] = min(dp[p.first][p.second + 1], dp[p.first][p.second] + 1);
+                labyrinth[p.first][p.second + 1] = 0;
             }
         }
 
