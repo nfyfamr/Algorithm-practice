4f83b2c7ea5bb06a512b0d2351117cd975378490
Yongjae Lee <nfyfamraa@gmail.com>
2024-02-05 18:43:50 -0500

    20955 - trial 3, find and union, TLE

--- www.acmicpc.net/20955/main.cpp
+++ www.acmicpc.net/20955/main.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 #include<vector>
-#include<utility>
-#include<algorithm>
+#include<numeric>
 
 using namespace std;
+
+int _find(int n, vector<int>& parent)
+{
+	// cout << n << parent[n] << " " << endl;
+    return n == parent[n] ? n : _find(parent[n], parent);
+}
+
+void _union(int fu, int fv, vector<int>& parent)
+{
+    parent[fv] = fu;
+    return;
+}
 
 int main()
 {
@@ -12,34 +23,29 @@ int main()
 
     int N, M;
     cin >> N >> M;
-    vector<pair<int,int>> edge;
+
+    int ops = 0;
+    vector<int> parent(N+1);
+    iota(parent.begin() + 1, parent.end(), 1);
+
     for (int i=0, u, v; i < M; ++i)
     {
         cin >> u >> v;
-        if (u < v)
-            edge.push_back({u, v});
-        else
-            edge.push_back({v, u});
+        int fu = _find(u, parent);
+        int fv = _find(v, parent);
+        if (fu == fv)
+        {
+            ops++;
+            parent[u] = parent[v] = fu;
+            continue;
+        }
+
+        _union(fu, fv, parent);
     }
 
-    sort(edge.begin(), edge.end(), [](auto& l, auto& r) {
-        return l.first < r.first;
-    });
+    for (int i=1; i<N+1; ++i)
+        if (parent[i] == i)
+            ops++;
 
-    vector<int> parent(N+1);
-    int ops = -1;
-    for (auto& e : edge)
-    {
-        if (parent[e.first] == 0 && parent[e.second] == 0)
-        {
-            ops++;  // connecting op.
-        }
-        else if (parent[e.first] != 0 && parent[e.second] != 0)
-        {
-            ops++;  // cutting op.
-        }
-        parent[e.first] = parent[e.second] = 1;
-    }
-
-    cout << ops;
+    cout << ops - 1;
 }
