f5af4c9c531f6e9c0305e15d28477b70dc928e00
Yongjae Lee <nfyfamraa@gmail.com>
2024-02-05 17:31:00 -0500

    20955 - trial 1, implementation

---
+++ www.acmicpc.net/20955/main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<utility>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+    vector<pair<int,int>> edge;
+    for (int i=0, u, v; i < M; ++i)
+    {
+        cin >> u >> v;
+        if (u < v)
+            edge.push_back({u, v});
+        else
+            edge.push_back({v, u});
+    }
+
+    sort(edge.begin(), edge.end(), [](auto& l, auto& r) {
+        return l.first < r.first;
+    });
+
+    vector<int> parent(N+1);
+    int ops = -1;
+    for (auto& e : edge)
+    {
+        if (parent[e.first] == 0)
+        {
+            parent[e.first] = e.first;
+            ops++;  // connecting op.
+        }
+        if (parent[e.first] != 0 && parent[e.second] != 0)
+        {
+            ops++;  // cutting op.
+        }
+        parent[e.second] = e.first;
+    }
+
+    cout << ops;
+}
