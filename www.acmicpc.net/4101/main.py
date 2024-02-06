4e8d39b1128e742bdf9a7bc7b17a7c0cdc7bf0bc
Yongjae Lee <nfyfamraa@gmail.com>
2024-02-06 11:22:31 -0500

    4101 - trial 2, implementation

--- www.acmicpc.net/4101/main.py
+++ www.acmicpc.net/4101/main.py
@@ -1,5 +1,9 @@
-for line in open(0):
-    a, b = map(int, line.strip().split())
+input = open(0).readline
+while True:
+    a, b = map(int, input().strip().split())
+    if a == 0 and b == 0:
+        break
+
     if a > b:
         print('Yes')
     else:
